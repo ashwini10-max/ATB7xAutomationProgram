9b0f8e08ece3be5af920cb6a8441b47257abadbd
ASHWINI10 <lodha.ashwini@gmail.com>
2024-10-13 17:19:37 +0530

    2files added
    Signed-off-by: ASHWINI10 <lodha.ashwini@gmail.com>

--- src/test/java/com/thetestingacademy/Test002.java
+++ src/test/java/com/thetestingacademy/Test002.java
@@ -1,4 +1,20 @@
 package com.thetestingacademy;
 
+import io.restassured.RestAssured;
+
 public class Test002 {
+    public static void main(String[] args) {
+        System.out.println("GET Request Demo");
+
+        RestAssured
+
+
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com/")
+                .basePath("/booking/1")
+                .when()
+                .get()
+                .then()
+                .statusCode(200);
+    }
 }
